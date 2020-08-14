61dbea2883af9d6c3e8e1f41ef702b75515be6c1
korybala <hoon073@g.skku.edu>
2020-08-14 17:06:06 +0900

    ALL DONE

--- src/com/sds/getMenu.java
+++ src/com/sds/getMenu.java
@@ -62,6 +62,87 @@ public class getMenu extends HttpServlet {
 			obj.put("stock", origin_stock - order_stock);
 		}
 		
+		String supply_data = request.getParameter("supply");
+		if (supply_data != null) {
+			int item_number_first_index = supply_data.indexOf('&');
+			int item_number_last_index = supply_data.indexOf('&',item_number_first_index + 1);
+			int item_number = Integer.parseInt(supply_data.substring(item_number_first_index + 1, item_number_last_index));
+			int att_number_first_index = supply_data.indexOf('%');
+			int att_number_last_index = supply_data.indexOf('%',att_number_first_index + 1);
+			int att_number = Integer.parseInt(supply_data.substring(att_number_first_index + 1, att_number_last_index));
+			ArrayList<Menu> list2 = new ArrayList<>();
+			
+			String id = "001";
+			int deep_number_first_index = supply_data.indexOf('@');
+			int deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			String name = supply_data.substring(deep_number_first_index + 1, deep_number_last_index);
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			int stock = Integer.parseInt(supply_data.substring(deep_number_first_index + 1, deep_number_last_index));
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			int price = Integer.parseInt(supply_data.substring(deep_number_first_index + 1, deep_number_last_index));
+			list2.add(new Menu(id, name, price, stock));
+			
+			id = "002";
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			name = supply_data.substring(deep_number_first_index + 1, deep_number_last_index);
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			stock = Integer.parseInt(supply_data.substring(deep_number_first_index + 1, deep_number_last_index));
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			price = Integer.parseInt(supply_data.substring(deep_number_first_index + 1, deep_number_last_index));
+			list2.add(new Menu(id, name, price, stock));
+			
+			id = "003";
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			name = supply_data.substring(deep_number_first_index + 1, deep_number_last_index);
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			stock = Integer.parseInt(supply_data.substring(deep_number_first_index + 1, deep_number_last_index));
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			price = Integer.parseInt(supply_data.substring(deep_number_first_index + 1, deep_number_last_index));
+			list2.add(new Menu(id, name, price, stock));
+			
+			id = "004";
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			name = supply_data.substring(deep_number_first_index + 1, deep_number_last_index);
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			stock = Integer.parseInt(supply_data.substring(deep_number_first_index + 1, deep_number_last_index));
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			price = Integer.parseInt(supply_data.substring(deep_number_first_index + 1, deep_number_last_index));
+			list2.add(new Menu(id, name, price, stock));
+			
+			id = "005";
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			name = supply_data.substring(deep_number_first_index + 1, deep_number_last_index);
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			stock = Integer.parseInt(supply_data.substring(deep_number_first_index + 1, deep_number_last_index));
+			deep_number_first_index = supply_data.indexOf('@',deep_number_last_index + 1);
+			deep_number_last_index = supply_data.indexOf('@',deep_number_first_index + 1);
+			price = Integer.parseInt(supply_data.substring(deep_number_first_index + 1, deep_number_last_index));
+			list2.add(new Menu(id, name, price, stock));
+			
+			ja.clear();
+			for(Menu m2:list2) {
+				JSONObject obj = new JSONObject();
+				obj.put("id", m2.getId());
+				obj.put("name", m2.getName());
+				obj.put("price", m2.getPrice());
+				obj.put("stock", m2.getStock());
+				ja.add(obj);
+			}
+		}
+		
 
 		response.setContentType("text/json;charset=euc-kr");
 		PrintWriter out = response.getWriter();
